e107acc80beba3ec20c5d6a2ed541597751c820f
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-02-24 16:33:32 -0300

    erros

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(){
     var $body = document.querySelector('body');
-    $body.classList.remove('no-js');
-    $body.classList.add('js');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
 
 
     var menu = new Menu({
@@ -9,4 +9,12 @@
         toggleBtn: '.header__btnMenu',
         widthEnabled: 1024
     })
-})();
+
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+})()
